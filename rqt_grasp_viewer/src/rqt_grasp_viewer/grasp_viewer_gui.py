2f6da4ba7fa6139a4b247b0122121480fe47dc09
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2017-07-07 18:02:35 +0200

    Added support for interactive_marker update from rqt grasp viewer

--- rqt_grasp_viewer/src/rqt_grasp_viewer/grasp_viewer_gui.py
+++ rqt_grasp_viewer/src/rqt_grasp_viewer/grasp_viewer_gui.py
@@ -34,11 +34,13 @@ from PyQt5.uic import loadUi
 from QtCore import Qt, QThread, pyqtSignal, QObject
 from QtGui import QStandardItemModel, QStandardItem
 from QtWidgets import QWidget, QTableView, QCheckBox, QFileDialog, \
-     QMessageBox, QPushButton, QFrame, QHBoxLayout, QVBoxLayout
+     QMessageBox, QPushButton, QFrame, QHBoxLayout, QVBoxLayout, QRadioButton
 
+from geometry_msgs.msg import TransformStamped, Vector3Stamped, Vector3, Point, PointStamped, Quaternion
 from grasping_msgs.msg import GraspPlanningActionResult, GenerateGraspsActionResult
-
 from grasp_viewer.srv import DisplayGrasps, DisplayGraspsRequest
+from visualization_msgs.msg import InteractiveMarkerFeedback as IM
+import tf2_geometry_msgs
 
 
 class GraspViewerGUI(Plugin):
@@ -69,12 +71,24 @@ class GraspViewerGUI(Plugin):
         main_layout.addWidget(self._table_view)
         self._widget.scrollarea.setLayout(main_layout)
 
+        self.radio = {}
+        for group, group_name in zip([self._widget.groupBoxGoal, self._widget.groupBoxStart],
+                                     ['goal', 'start']):
+            vl = QVBoxLayout()
+            self.radio[group_name] = {}
+            for button in ['grasp', 'approach', 'retreat']:
+                self.radio[group_name][button] = QRadioButton()
+                self.radio[group_name][button].setText(button)
+                vl.addWidget(self.radio[group_name][button])
+            group.setLayout(vl)
+
         self.init_services()
         self.init_subscribers()
+        self.init_publishers()
 
         # self._table_view.clicked.connect(self.on_table_view_clicked)
         self._table_view.selectionModel().selectionChanged.connect(self.on_table_view_select)
-        #self._widget.treeWidget.itemClicked.connect(self.on_posture_clicked)
+        # self._widget.treeWidget.itemClicked.connect(self.on_posture_clicked)
 
     def init_services(self):
         """
@@ -96,7 +110,10 @@ class GraspViewerGUI(Plugin):
         self._grasp_sub = rospy.Subscriber("/grasp_manager/result", GraspPlanningActionResult,
                                            self.graspable_result_cb)
         self._grasp_sub2 = rospy.Subscriber("/grasp_provider/result", GenerateGraspsActionResult,
-                                           self.graspable_result_cb)
+                                            self.graspable_result_cb)
+
+    def init_publishers(self):
+        self._int_marker = rospy.Publisher("/rviz_moveit_motion_planning_display/robot_interaction_interactive_marker_topic/feedback", IM, queue_size=2)
 
     def graspable_result_cb(self, msg):
         result = msg.result
@@ -126,15 +143,52 @@ class GraspViewerGUI(Plugin):
             item_grasp_quality.setEditable(False)
             fm.appendRow([item_id, item_obj, idem_grasp_id, item_grasp, item_grasp_quality])
         self._table_view.resizeColumnsToContents()
-        #fm = self._filemodel[group_name]
-        #item_idx = fm.index(idx, 0)
-        #fm.setData(item_idx, str(time_from_start))
+        # fm = self._filemodel[group_name]
+        # item_idx = fm.index(idx, 0)
+        # fm.setData(item_idx, str(time_from_start))
 
     def on_table_view_clicked(self, index):
 
         items = self._table_view.selectedIndexes()
-        for it in items:
-            print 'selected item index found at %s' % it.row()
+        # for it in items:
+        #    print 'selected item index found at %s' % it.row()
+
+    def make_marker_grasp(self, marker, last_grasp_id):
+        marker.pose = self.grasps[last_grasp_id].grasp_pose.pose
+
+    def make_marker_approach(self, marker, last_grasp_id):
+        approach = self.grasps[last_grasp_id].pre_grasp_approach.direction
+        approach_as_point = PointStamped()
+        approach_as_point.point = Point(approach.vector.x, approach.vector.y, approach.vector.z)
+        approach_as_point.point.x *= -1 * self.grasps[last_grasp_id].pre_grasp_approach.desired_distance
+        approach_as_point.point.y *= -1 * self.grasps[last_grasp_id].pre_grasp_approach.desired_distance
+        approach_as_point.point.z *= -1 * self.grasps[last_grasp_id].pre_grasp_approach.desired_distance
+
+        t = TransformStamped()
+        t.transform.translation = Vector3(self.grasps[last_grasp_id].grasp_pose.pose.position.x,
+                                          self.grasps[last_grasp_id].grasp_pose.pose.position.y,
+                                          self.grasps[last_grasp_id].grasp_pose.pose.position.z)
+        t.transform.rotation = Quaternion(self.grasps[last_grasp_id].grasp_pose.pose.orientation.x,
+                                          self.grasps[last_grasp_id].grasp_pose.pose.orientation.y,
+                                          self.grasps[last_grasp_id].grasp_pose.pose.orientation.z,
+                                          self.grasps[last_grasp_id].grasp_pose.pose.orientation.w)
+
+        new_approach_as_point = tf2_geometry_msgs.do_transform_point(approach_as_point, t)
+        marker.pose = deepcopy(self.grasps[last_grasp_id].grasp_pose.pose)
+        marker.pose.position = new_approach_as_point.point
+
+    def make_marker_retreat(self, marker, last_grasp_id):
+        retreat = self.grasps[last_grasp_id].post_grasp_retreat.direction
+        retreat_as_point = PointStamped()
+        retreat_as_point.point = Point(retreat.vector.x, retreat.vector.y, retreat.vector.z)
+        retreat_as_point.point.x *= self.grasps[last_grasp_id].post_grasp_retreat.desired_distance
+        retreat_as_point.point.y *= self.grasps[last_grasp_id].post_grasp_retreat.desired_distance
+        retreat_as_point.point.z *= self.grasps[last_grasp_id].post_grasp_retreat.desired_distance
+
+        marker.pose = deepcopy(self.grasps[last_grasp_id].grasp_pose.pose)
+        marker.pose.position.x += retreat_as_point.point.x
+        marker.pose.position.y += retreat_as_point.point.y
+        marker.pose.position.z += retreat_as_point.point.z
 
     def on_table_view_select(self, selected, deselected):
         fm = self._filemodel
@@ -144,6 +198,7 @@ class GraspViewerGUI(Plugin):
                 rospy.logwarn("More than 5 items selected, \
                               but grasp viewer can only display 5 grasps at the same time")
             req = DisplayGraspsRequest()
+            last_grasp_id = None
             for it in items:
                 row = it.row()
                 obj_idx = fm.index(it.row(), 0)
@@ -151,11 +206,33 @@ class GraspViewerGUI(Plugin):
                 grasp_idx = fm.index(it.row(), 2)
                 grasp_id = int(fm.data(grasp_idx))
                 req.grasps.append(self.grasps[grasp_id])
-                #print "selected item index found at ", obj_id, grasp_id
+                last_grasp_id = grasp_id
+                # print "selected item index found at ", obj_id, grasp_id
             try:
                 self._grasp_viz_client(req)
             except Exception as e:
                 print e
+
+            # the approach is in frame ee_link, retrieve it from there
+            ee_link = self.grasps[last_grasp_id].pre_grasp_approach.direction.header.frame_id
+            markerFeedBack = IM()
+
+            markerFeedBack.header.frame_id = "base_link"
+            markerFeedBack.control_name = "move"
+            markerFeedBack.event_type = IM.POSE_UPDATE
+
+            for checkbox, name in zip([self._widget.int_mk_goal_chk, self._widget.int_mk_start_chk],
+                                      ['goal', 'start']):
+                if checkbox.isChecked():
+                    markerFeedBack.marker_name = "EE:" + name + "_" + ee_link
+                    if self.radio[name]['grasp'].isChecked():
+                        self.make_marker_grasp(markerFeedBack, last_grasp_id)
+                    if self.radio[name]['approach'].isChecked():
+                        self.make_marker_approach(markerFeedBack, last_grasp_id)
+                    if self.radio[name]['retreat'].isChecked():
+                        self.make_marker_retreat(markerFeedBack, last_grasp_id)
+                    self._int_marker.publish(markerFeedBack)
+                    # print markerFeedBack
 
     #########
     # Default methods for the rqtgui plugins
